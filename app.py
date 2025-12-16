c3186657eb63c6b577054e7cc8e3e5482ccc02ef
xongs <xonguinhos@gmail.com>
2025-12-16 10:05:48 -0300

    no large uploads

--- app.py
+++ app.py
@@ -114,17 +114,13 @@ scheduled_tasks = {}
 async def lukinhas(ctx):
 	# lukinhas usa o comando
 	if ctx.author.id == lukinhas_id:
-		f = open("assets/john-cena-looking-downwards.gif", "rb")
-		gif = discord.File(f)
-		await ctx.reply("Lukinhas, você é um bobinho :stuck_out_tongue_closed_eyes::rofl:", file=gif)
+		await ctx.reply("Lukinhas, você é um bobinho :stuck_out_tongue_closed_eyes::rofl:", embed="https://cdn.discordapp.com/attachments/1450470865509679114/1450471003640828015/john-cena-looking-downwards.gif?ex=6942a7e3&is=69415663&hm=48b765f095b5c1eb83e2453e8a8d2573bd8af3c80648b58becdd1964a547a782&")
 		return
 
 	# alguem que nao seja nem eu nem loren
 	allowlist = [909210394139168838, 754371726498070568]
 	if ctx.author.id not in allowlist:
-		f = open("assets/bluezao-macaco.gif", "rb")
-		gif = discord.File(f)
-		await ctx.reply("Boa fdp, ta achando que vai mutar o Lukinhas :clown:? Você não manda em nada aqui não cuzão :rofl:", file=gif)
+		await ctx.reply("Boa fdp, ta achando que vai mutar o Lukinhas :clown:? Você não manda em nada aqui não cuzão :rofl:", embed="https://cdn.discordapp.com/attachments/1450470865509679114/1450471002411765911/bluezao-macaco.gif?ex=6942a7e3&is=69415663&hm=355db46c5d6714a7022e2e68b6ec4f6f57ff1bf1fc21f4a09a87b8f77660feec&")
 		return
 
 	member = ctx.guild.get_member(lukinhas_id) or await ctx.guild.fetch_member(lukinhas_id) # member = lukinhas
@@ -163,24 +159,18 @@ async def lukinhas_on_error(ctx, error):
 # !loren
 @bot.command()
 async def loren(ctx):
-	f = open("assets/renan-putasso.mp3", "rb")
+	f = open("media/renan-putasso.mp3", "rb")
 	audio = discord.File(f)
 	await ctx.reply(f"{ctx.author.mention} mandou um recado pro loren:", file=audio)
+	return
 
 # !fabgodamn
 @bot.command()
 async def fabgodamn(ctx):
-    import os, random, discord
-
-    path = "./assets/!fabgodamn/"
-    videos = [f for f in os.listdir(path) if f.lower().endswith(".mp4")]
-
-    if videos:
-        escolhido = videos[random.randint(0, len(videos) - 1)]
-        arquivo = os.path.join(path, escolhido)
-        with open(arquivo, "rb") as f:
-            await ctx.reply("FABGODAMN", file=discord.File(f))
-
+	videos = ["https://cdn.discordapp.com/attachments/1450470865509679114/1450470932195053569/12162.mp4?ex=6942a7d2&is=69415652&hm=e64e4c0604d150712116d40a2413e9755681daca0e304d62fd394205f37e931f&", "https://cdn.discordapp.com/attachments/1450470865509679114/1450470932907954226/12163.mp4?ex=6942a7d2&is=69415652&hm=94e2877df6bd2709cb47f6348def5263f5adbc2a5183fbcaa12acd18dfa12c45&", "https://cdn.discordapp.com/attachments/1450470865509679114/1450470933973438586/12164.mp4?ex=6942a7d3&is=69415653&hm=e843b73eae84e6938b3eebc3550d063463b9430b0ac82d26a4ba64e1135a7a34&", "https://cdn.discordapp.com/attachments/1450470865509679114/1450470934925410354/12165.mp4?ex=6942a7d3&is=69415653&hm=784c65cadc7a6a04816cebc3caf5ac7e17c88e86fba122d8871d512faf21cb2d&", "https://cdn.discordapp.com/attachments/1450470865509679114/1450470935697166457/12166.mp4?ex=6942a7d3&is=69415653&hm=76f01e4876ae96ae82cacec09d49b91304150c54c8b756966b4a60b74586de91&", "https://cdn.discordapp.com/attachments/1450470865509679114/1450470936586489856/12167.mp4?ex=6942a7d3&is=69415653&hm=03a8fb69039c24e2d4a183db3209d0c5f8f6e1aee9c332eacf57e796ac668114&", "https://cdn.discordapp.com/attachments/1450470865509679114/1450470943049646180/12168.mp4?ex=6942a7d5&is=69415655&hm=60fb7d21c31d9921e05a1d9cf880a6f88c792b222ddb6c15c58d7afd4bb101b7&", "https://cdn.discordapp.com/attachments/1450470865509679114/1450470944677298248/12169.mp4?ex=6942a7d5&is=69415655&hm=0653b5e9909699cd39afd78e931c3332bb7211f5f79fa4723c12dbced33834fc&", "https://cdn.discordapp.com/attachments/1450470865509679114/1450470945788657808/12161.mp4?ex=6942a7d5&is=69415655&hm=4290d0fda1a176aad81b119c30b274c53e48f56baaa4b02d51acdae85e5ab2ef&", "https://cdn.discordapp.com/attachments/1450470865509679114/1450470962200973332/1216.mp4?ex=6942a7d9&is=69415659&hm=c8194e3a098a2e8bb9312ef51a743c9b1a31341dbc84cc382247091022aeb945&", "https://cdn.discordapp.com/attachments/1450470865509679114/1450470962914132100/121610.mp4?ex=6942a7da&is=6941565a&hm=65ecebab3adb6010dc46c57458982e760c4fd70f49c835365ece63d3b123a369&"]
+	video = videos[random.randint(0, len(videos) - 1)]
+	await ctx.reply(f"FABGODAMN\n{video}")
+	return
 
 if __name__ == "__main__":
 	# keep_alive()
