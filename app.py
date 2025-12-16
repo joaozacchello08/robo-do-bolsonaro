56947201852da32b94476b571e9c6650536f720b
xongs <xonguinhos@gmail.com>
2025-12-15 23:52:38 -0300

    .

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
+from keep_alive import keep_alive
 from dotenv import load_dotenv
 import os
-from keep_alive import keep_alive
 import discord
 from discord.ext import commands
 import logging
+import asyncio
 
 load_dotenv()
 token = os.getenv("DISCORD_TOKEN")
@@ -11,7 +12,7 @@ token = os.getenv("DISCORD_TOKEN")
 keep_alive()
 
 handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
-intents = discord.Intents.default()
+intents = discord.Intents.all()
 intents.message_content = True
 intents.members = True
 
@@ -19,6 +20,7 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 
 role_id = 1450224602893652029 # main server role
 test_role_id = 1450294816888852480
+id_lukinhas = 837599708235038730
 
 @bot.event
 async def on_ready():
@@ -34,5 +36,26 @@ async def on_member_join(member):
 		await member.add_roles(test_role)
 	else:
 		print("Couldn't find and assign the role.")
+
+@bot.command()
+async def lukinhas(ctx):
+	member = ctx.guild.get_member(id_lukinhas)
+
+	if not member:
+		await ctx.reply(f"Usuário Lukinhas (ID: {id_lukinhas}) não encontrado.")
+		return
+
+	if not member.voice:
+		await ctx.reply(f"Porra {ctx.author.name}, o Lukinhas nem tá na call :rofl::rofl:", mention_author=False)
+		return
+
+	# mutar
+	await member.edit(mute=True)
+	await ctx.send(f"Lukinhas mutado com sucesso, desmutando em 30 segundos. {member.mention}")
+
+	# esperar, desmutar
+	await asyncio.sleep(30)
+	await member.edit(mute=False)
+	await member.send(f"Lukinhas desmutado. {ctx.author.mention}")
 
 bot.run(token, log_handler=handler, log_level=logging.DEBUG)
