2355b919525549e3e9d02fa61a6079d519e3c887
xongs <xonguinhos@gmail.com>
2025-12-16 00:06:22 -0300

    .

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ token = os.getenv("DISCORD_TOKEN")
 keep_alive()
 
 handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
-intents = discord.Intents.all()
+intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
