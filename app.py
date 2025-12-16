46f03ccaf3f1d890abe53c443ba51560d686082d
xongs <xonguinhos@gmail.com>
2025-12-16 07:01:54 -0300

    autorole concluded

--- app.py
+++ app.py
@@ -4,13 +4,12 @@ import os
 import discord
 from discord.ext import commands
 import logging
-import asyncio
+import sqlite3
 
 load_dotenv()
 token = os.getenv("DISCORD_TOKEN")
 
-keep_alive()
-
+# keep_alive()
 handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
 intents = discord.Intents.default()
 intents.message_content = True
@@ -18,65 +17,87 @@ intents.members = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-role_id = 1450224602893652029 # main server role
-test_role_id = 1450294816888852480
-lukinhas_id = 837599708235038730
-adm_role = 1423426169381978173
-loren_id = 754371726498070568
-xongs_id = 909210394139168838
+#region setting up db
+filename = "guild_data.db"
+con = sqlite3.connect(filename)
+cur = con.cursor()
+
+async def init_guild_data(guild):
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS guilds(
+            guild_id INTEGER PRIMARY KEY,
+            autorole_id INTEGER DEFAULT NULL
+        )
+    """)
+    con.commit()
+
+    cur.execute("""
+        INSERT OR IGNORE INTO guilds(guild_id)
+        VALUES (?)
+    """, (guild.id,))
+    con.commit()
 
 @bot.event
 async def on_ready():
-	print(f"Bot is running, {bot.user.name} is ready!")
-	return
+	for g in bot.guilds:
+		await init_guild_data(g)
+	print(f"O bot tá rodando nessa porra, {bot.user.name} tá pras foda!")
+
+@bot.event
+async def on_guild_join(g):
+	await init_guild_data(g)
+#endregion
+
+#region autorole
+@bot.command()
+@commands.has_permissions(administrator=True)
+async def setautorole(ctx, role_mention: str):
+	try:
+		role_id = int(role_mention.strip("<@&>"))
+		role = ctx.guild.get_role(role_id)
+		if not role:
+			await ctx.reply("Cargo não encontrado.")
+			return
+	except ValueError:
+		await ctx.reply("Formato de menção inválido.")
+		return
+	
+	cur.execute("""
+		UPDATE guilds
+	    SET autorole_id = ?
+		WHERE guild_id = ?;
+	""", (role_id, ctx.guild.id))
+	con.commit()
+
+	await ctx.reply(f"O cargo (ID) {role_id} foi definido como autorole!")
+
+@setautorole.error
+async def set_autorole_error(ctx, error):
+	if isinstance(error, commands.MissingPermissions):
+		await ctx.reply("Você não tem permissão pra isso.")
+	elif isinstance(error, commands.BadArgument):
+		await ctx.reply("Cargo inválido.")
+	else:
+		raise error
 
 @bot.event
 async def on_member_join(member):
-	role = member.guild.get_role(role_id)
-	test_role = member.guild.get_role(test_role_id)
-	if role:
-		await member.add_roles(role)
-		return
-	elif not role:
-		await member.add_roles(test_role)
-		return
-	else:
-		print("Couldn't find and assign the role.")
-		return
+	res = cur.execute(f"SELECT autorole_id FROM guilds WHERE guild_id = {member.guild.id}")
+	role_id = res.fetchone()
+	if role_id:
+		role_id = role_id[0]
+		role = member.guild.get_role(role_id)
+		if role:
+			await member.add_roles(role)
+#endregion
+
 
 @bot.command()
 async def ping(ctx):
-	await ctx.reply("pong")
+	await ctx.reply("pong :ping_pong:")
 
 @bot.command()
-# @commands.has_role(adm_role)
 async def lukinhas(ctx):
-	if ctx.author.id not in [loren_id, xongs_id, lukinhas_id]: # not working
-		file = discord.File("assets/bluezao-macaco.gif", filename="bluezao-puto-com-macaquisse.gif")
-		await ctx.reply("Tá querendo mutar o MEU Lukinhas sem ser adm? Você não manda em nada aqui porra. ", file=file)
-		return
-
-	if ctx.author.id == lukinhas_id:
-		file = discord.File("assets/john-cena-looking-downwards.gif", filename="john-cena-decepcionado-com-macaquisse.gif")
-		await ctx.reply("Lukinhas, você é um bobinho :rofl::rofl:", file=file)
-		return
-
-	member = ctx.guild.get_member(lukinhas_id) # member = lukinhas
-	if not member:
-		await ctx.reply(f"Usuário Lukinhas (ID: {lukinhas_id}) não encontrado.")
-		return
-
-	if not member.voice:
-		await ctx.reply(f"Porra {ctx.author.name}, o Lukinhas nem tá na call :rofl::rofl:")
-		return
-
-	# mutar
-	await member.edit(mute=True)
-	await ctx.reply(f"Lukinhas mutado com sucesso, desmutando em 15 segundos. {ctx.author.mention}")
-
-	# esperar, desmutar
-	await asyncio.sleep(15)
-	await member.edit(mute=False)
-	await ctx.send(f"Lukinhas ~~infelizmente~~ desmutado. {ctx.author.mention} {member.mention}")
+	pass
 
 bot.run(token, log_handler=handler, log_level=logging.DEBUG)
